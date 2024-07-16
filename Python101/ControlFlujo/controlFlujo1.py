60112d9af01fce46d29787f069f5538250519497
jobarv <jbarrera@motiontech.com.mx>
2024-07-15 21:59:48 -0600

    se agrega control de flujo

---
+++ Python101/ControlFlujo/controlFlujo1.py
@@ -0,0 +1,19 @@
+edad: int = 17
+
+if edad < 18:
+    print("No estas autorizado para manejar.")
+else:
+    print("Aqui esta tu licencia  de manejo")
+
+
+edad = 15
+# 16 < edad < 100
+if edad < 16:
+    print("Tu no estas autorizado para manejar")
+elif edad >=100:
+    print("Eres muy viejo para manejar")
+else:
+    print("Aqui esta tu licencia")
+
+
+
